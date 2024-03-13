f6967b732b70da4b065f16172aa465cd82f45cf4
Number <number9294@naver.com>
2024-03-13 11:35:36 +0900

    추가 .gitignore

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/static',express.static("public"));
+
+app.get('/', (req, res) => {
+    res.send("Hello, Express.js");
+})
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
